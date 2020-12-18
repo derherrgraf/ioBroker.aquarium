ed634703700dbaa3bce4694c94d886aa93b26ee5
Timo Wiese <timo-wiese@gmx.net>
2020-12-18 15:31:46 +0100

    init adapter

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"aquarium adapter settings": {
+		"en": "Adapter settings for aquarium",
+		"de": "Adaptereinstellungen für aquarium",
+		"ru": "Настройки адаптера для aquarium",
+		"pt": "Configurações do adaptador para aquarium",
+		"nl": "Adapterinstellingen voor aquarium",
+		"fr": "Paramètres d'adaptateur pour aquarium",
+		"it": "Impostazioni dell'adattatore per aquarium",
+		"es": "Ajustes del adaptador para aquarium",
+		"pl": "Ustawienia adaptera dla aquarium",
+		"zh-cn": "aquarium的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
